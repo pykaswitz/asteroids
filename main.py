43751f2ca157314de1c5a29e82b634c2c0805184
pykaswitz <bwjl03@gmail.com>
2025-07-26 21:51:44 -0600

    created black screen and ability to close game window

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame
-import constants
+import pygame as pyg
+import constants as cons
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print(f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {cons.SCREEN_WIDTH}")
+    print(f"Screen height: {cons.SCREEN_HEIGHT}")
+
+    # Use pygame's display.set_mode() to get a new GUI window
+    screen = pyg.display.set_mode((cons.SCREEN_WIDTH, cons.SCREEN_HEIGHT))
+
+    while True:
+        for event in pyg.event.get():       #This will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
+            if event.type == pyg.QUIT:
+                return
+        screen.fill((0,0,0))
+        pyg.display.flip()
 
 
 if __name__ == "__main__":
