a7dcfac10b6222deae155ed5e194b1afd886312e
pykaswitz <bwjl03@gmail.com>
2025-07-28 18:37:37 -0600

    updated contants, created player.py and circleshape.py, updated main to drawer player/triangle

--- main.py
+++ main.py
@@ -1,27 +1,35 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
-import pygame as pyg
-import constants as cons
+import pygame
+import constants
+import player
 
 def main():
     print("Starting Asteroids!")
-    print(f"Screen width: {cons.SCREEN_WIDTH}")
-    print(f"Screen height: {cons.SCREEN_HEIGHT}")
+    print(f"Screen width: {constants.SCREEN_WIDTH}")
+    print(f"Screen height: {constants.SCREEN_HEIGHT}")
 
     # Use pygame's display.set_mode() to get a new GUI window
-    screen = pyg.display.set_mode((cons.SCREEN_WIDTH, cons.SCREEN_HEIGHT))
-    clock = pyg.time.Clock()
+    screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
     dt = 0
+    x = constants.SCREEN_WIDTH / 2
+    y = constants.SCREEN_HEIGHT / 2
+    my_player = player.Player(x, y)
 
     while True:
-        for event in pyg.event.get():       # This will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
-            if event.type == pyg.QUIT:
+        for event in pygame.event.get():
+            # This will check if the user has closed the window and exit the game loop if they do. It will make the window's close button work.
+            if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0))                # creates a window using screen obj and fills it solid black
-        pyg.display.flip()
+        screen.fill((0,0,0))
+        # creates a window using screen obj and fills it solid black
+        my_player.draw(screen)
+        pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick(60) / 1000          # sets screen refresh to 60fps and updates dt (delta time) from miliseconds to seconds
+        dt = clock.tick(60) / 1000
+        # sets screen refresh to 60fps and updates dt (delta time) from miliseconds to seconds
 
 
 if __name__ == "__main__":
---
+++ player.py
@@ -0,0 +1,20 @@
+import pygame
+import circleshape
+import constants
+
+class Player (circleshape.CircleShape):
+    def __init__(self, x, y):
+        super().__init__(x, y, constants.PLAYER_RADIUS)
+        self.rotation = 0
+
+    # in the player class / A player will look like a triangle, even though we'll use a circle to represent its hitbox
+    def triangle(self):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
+        a = self.position + forward * self.radius
+        b = self.position - forward * self.radius - right
+        c = self.position - forward * self.radius + right
+        return [a, b, c]
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), width=2)
